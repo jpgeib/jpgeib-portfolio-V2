b8c2cc436894429fe5efd48c709190693ae51862
James Geib <jpgeib2@gmail.com>
2024-12-14 14:53:54 -0500

    created initial page for single Review

---
+++ client/src/pages/Review/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+import "./style.css";
+
+const Review = () => {
+    return (
+        <>
+            <Header as="h1" id="review-header">Review</Header>
+        </>
+    );
+};
+
+export default Review;
