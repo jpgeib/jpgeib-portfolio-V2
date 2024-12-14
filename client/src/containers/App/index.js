af6193ce647d55d6d26e8dbae1f0f8629c86330a
James Geib <jpgeib2@gmail.com>
2024-12-14 14:18:42 -0500

    rearranged src files into components and containers folders, moved App.js into containers folder and renamed to index.js within App subfolder

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import "./styles.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  );
+}
+
+export default App;
