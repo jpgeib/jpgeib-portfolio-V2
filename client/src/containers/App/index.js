3d42b6bb0285ddeb6f4ba7c5b44567dc5ee562ca
James Geib <jpgeib2@gmail.com>
2024-12-14 14:21:33 -0500

    added getWindowDimensions, useWindowDimensions, and useCurrentUrl methods to App

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -3,7 +3,47 @@ import { Route, Routes } from "react-router-dom";
 
 import "./styles.css";
 
+const getWindowDimensions = () => {
+  const page = document.querySelector("html");
+  const { clientWidth: width, clientHeight: height } = page;
+  return {
+    width,
+    height
+  }
+};
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export const useCurrentUrl = () => {
+  const [currentUrl, setCurrentUrl] = useState(window.location.href);
+
+  useEffect(() => {
+    const handlePageLoad = () => {
+      setCurrentUrl(window.location.href);
+    }
+    window.addEventListener("load", handlePageLoad);
+    return window.removeEventListener("load", handlePageLoad);
+  }, []);
+
+  return currentUrl;
+};
+
 const App = () => {
+
+  const { width } = useWindowDimensions();
+
   return (
     <>
       <h1>App</h1>
