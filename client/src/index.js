19990868a78d076f32a174112f86859426835409
James Geib <jpgeib2@gmail.com>
2025-08-07 17:23:13 -0400

    implemented AuthContextProvider, HelmetProvider, and ScrollToTop into root index.js for client

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContextProvider } from "./context/authContext";
+import ScrollToTop from "./components/ScrollToTop";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <HelmetProvider>
+    <Router>
+      <ScrollToTop />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Router>
+  </HelmetProvider>
 );
 
 reportWebVitals();
